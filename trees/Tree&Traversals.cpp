f65183855c3336c71e81e70bf13f666ddcc4f2aa
Tarushi Pathak <48761201+tarushi98@users.noreply.github.com>
2021-08-09 22:53:56 +0530

    Update and rename Tree&PreOrder.cpp to Tree&Traversals.cpp

--- trees/Tree&Traversals.cpp
+++ trees/Tree&Traversals.cpp
@@ -33,10 +33,29 @@ void print(node* root)
     print(root->left);
     print(root->right);
 }
+void inorder(node* root)
+{
+    if(root == NULL)
+        return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void postorder(node* root)
+{
+    if(root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
 int main() {
     
       node* root = buildtree();
       print(root);
       cout<<endl;
+      inorder(root);
+      cout<<endl;
+      postorder(root);
       return 0;
 }
